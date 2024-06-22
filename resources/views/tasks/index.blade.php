0d639d7b29dd5eb268a5fa57c176cd0e1253f8a9
vitus121289 <vmtusing@gmail.com>
2024-06-22 11:52:36 +0800

    Added html tags and blade directives to render tasks.

---
+++ resources/views/tasks/index.blade.php
@@ -0,0 +1,24 @@
+<x-app-layout>
+    <div class="mt-6 bg-white shadow-sm rounded-lg divide-y">
+        @foreach ($tasks as $task)
+            <div class="p-6 flex space-x-2">
+                <svg xlmns="http://www.w3.org/2000/svg" class="h-6 w-6 text-gray-600 -scale-100" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M8 12h.01M12 12h.01M16 12h.01M21 12c0 4.418-4.03 8-9 8a9.863 9.863 0 01-4.255-.949L3 20l1.395-3.72C3.512 15.042 3 13.574 3 12c0-4.418 4.03-8 9-8s9 3.582 9 8z"/>
+                </svg>
+            </div>
+            <div class="flex-1">
+                <div class="flex justify-between items-center">
+                    <div>
+                        <span class="text-gray-800">{{ $task->user->name }}</span>
+                        <small class="ml-2 text-sm text-gray-600">{{ $task->created_at->format('j M Y, g:i a') }}</small>
+                        {{-- @unless ($task->created_at->eq($task->updated_at))
+                            <small class="text-sm text-gray-600"> &middot; {{ __('edited') }}</small>
+                        @endunless --}}
+                    </div>
+                </div>
+                <h2>{{ $task->name }}</h2>
+                <p class="mt-4 text-lg text-gray-900">{{ $task->description }}</p>
+            </div>
+        @endforeach
+    </div>
+</x-app-layout>
