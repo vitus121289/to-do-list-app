7aca0c270bd920891151ac8bf1477b14739a54d7
vitus121289 <vmtusing@gmail.com>
2024-06-22 12:26:05 +0800

    Moved the div containing the name, and description of the task to the div containing the svg icon.

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -19,19 +19,19 @@
                     <svg xlmns="http://www.w3.org/2000/svg" class="h-6 w-6 text-gray-600 -scale-100" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
                         <path stroke-linecap="round" stroke-linejoin="round" d="M8 12h.01M12 12h.01M16 12h.01M21 12c0 4.418-4.03 8-9 8a9.863 9.863 0 01-4.255-.949L3 20l1.395-3.72C3.512 15.042 3 13.574 3 12c0-4.418 4.03-8 9-8s9 3.582 9 8z"/>
                     </svg>
-                </div>
-                <div class="flex-1">
-                    <div class="flex justify-between items-center">
-                        <div>
-                            <span class="text-gray-800">{{ $task->user->name }}</span>
-                            <small class="ml-2 text-sm text-gray-600">{{ $task->created_at->format('j M Y, g:i a') }}</small>
-                            {{-- @unless ($task->created_at->eq($task->updated_at))
-                                <small class="text-sm text-gray-600"> &middot; {{ __('edited') }}</small>
-                            @endunless --}}
+                    <div class="flex-1">
+                        <div class="flex justify-between items-center">
+                            <div>
+                                <span class="text-gray-800">{{ $task->user->name }}</span>
+                                <small class="ml-2 text-sm text-gray-600">{{ $task->created_at->format('j M Y, g:i a') }}</small>
+                                {{-- @unless ($task->created_at->eq($task->updated_at))
+                                    <small class="text-sm text-gray-600"> &middot; {{ __('edited') }}</small>
+                                @endunless --}}
+                            </div>
                         </div>
+                        <h2 class="mt-4 text-lg font-bold text-gray-900">{{ $task->name }}</h2>
+                        <p class="mt-4 text-lg text-gray-900">{{ $task->description }}</p>
                     </div>
-                    <h2>{{ $task->name }}</h2>
-                    <p class="mt-4 text-lg text-gray-900">{{ $task->description }}</p>
                 </div>
             @endforeach
         </div>
