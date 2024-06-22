5be3d91351379f45f31c8679e25f9e087c2ec6a4
vitus121289 <vmtusing@gmail.com>
2024-06-22 11:44:27 +0800

    Added columns for foreign ID, name of task, and description of task.

--- database/migrations/2024_06_22_030207_create_tasks_table.php
+++ database/migrations/2024_06_22_030207_create_tasks_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('tasks', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->constrained()->cascadeOnDelete();
+            $table->string('name');
+            $table->string('description');
             $table->timestamps();
         });
     }
